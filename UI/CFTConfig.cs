db0f024113892ffc959809990b2938d10ca80da9
Exomanz <74020511+Exomanz@users.noreply.github.com>
2020-12-18 12:51:28 -0600

    updating old files

--- UI/CFTConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CustomFailText.UI
-{
-    public class CFTConfig
-    {
-        public static CFTConfig Instance { get; set; }
-        public bool PluginToggle { get; set; } = true;
-        public bool PluginEnable { get; internal set; }
-    }
-}
